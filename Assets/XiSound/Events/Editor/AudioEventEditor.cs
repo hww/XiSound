9517888f06aaebd5606f48e7ecf9c635b13783fc
Valery <valery.hww@gmail.com>
2022-09-19 22:15:26 +0300

    fix(update): Updated

---
+++ Assets/XiSound/Events/Editor/AudioEventEditor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+namespace XiSound.Events.Editor
+{
+	/// <summary>
+	/// The sound event editor tool
+	/// </summary>
+	[CustomEditor(typeof(BaseAudioEvent), true)]
+	public class AudioEventEditor : UnityEditor.Editor
+	{
+
+		[SerializeField] private AudioSource _previewer;
+
+		public void OnEnable()
+		{
+			_previewer = EditorUtility.CreateGameObjectWithHideFlags("Audio preview", HideFlags.HideAndDontSave, typeof(AudioSource)).GetComponent<AudioSource>();
+		}
+
+		public void OnDisable()
+		{
+			DestroyImmediate(_previewer.gameObject);
+		}
+
+		public override void OnInspectorGUI()
+		{
+			DrawDefaultInspector();
+
+			EditorGUI.BeginDisabledGroup(serializedObject.isEditingMultipleObjects);
+			if (GUILayout.Button("Preview"))
+			{
+				((BaseAudioEvent) target).Play(_previewer);
+			}
+			EditorGUI.EndDisabledGroup();
+		}
+	}
+}
---
+++ Assets/XiSound/MusicManager.cs
@@ -0,0 +1,101 @@
+﻿// =============================================================================
+// MIT License
+// 
+// Copyright (c) 2018 Valeriya Pudova (hww.github.io)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// =============================================================================
+
+using XiSubsystem;
+using XiSound.Events;
+using UnityEngine;
+
+namespace XiSound
+{
+	public class MusicManager : BaseSystem
+	{
+		public static MusicManager Instance;
+    
+		private static SoundHandle musicHandle;
+		private static bool autoPlayNext;
+		private static float musicVolume;
+		private static AudioEvent MusicListAudioEvent;
+
+		public MusicManager(BaseSystem parent, AudioEvent audioEvent)
+		{
+			Instance = this;
+			parent?.AddChild(this);
+			MusicListAudioEvent = audioEvent;
+		}
+	
+		public static SoundHandle PlayMusic()
+		{
+			StopMusic();
+			autoPlayNext = true;
+			musicHandle = SoundSystem.Play(MusicListAudioEvent, onChangeState: OnMusicChangeState);
+			var soundSource = SoundSystem.GetSource(musicHandle);
+			Instance.PostMessage(Instance,(int)SoundSystemEvent.MusicManagerChangeMusic, soundSource.EventName, soundSource.ClipName);
+			return musicHandle;
+		}
+
+		public static SoundHandle PlayMusic(string clipName)
+		{
+			Debug.Assert(clipName != null);
+			StopMusic();
+			autoPlayNext = true;
+			musicHandle = SoundSystem.Play(MusicListAudioEvent, onChangeState: OnMusicChangeState);
+			var soundSource = SoundSystem.GetSource(musicHandle); 
+			Instance.PostMessage(Instance, (int)SoundSystemEvent.MusicManagerChangeMusic, soundSource.EventName, soundSource.ClipName);
+			return musicHandle;
+		}
+
+		public static void StopMusic()
+		{
+			autoPlayNext = false;
+			var soundSource = SoundSystem.GetSource(musicHandle);
+			if (soundSource != null && soundSource.IsNotCompleted)
+			{
+				Instance.PostMessage(Instance, (int)SoundSystemEvent.MusicManagerStopMusic, soundSource.EventName, soundSource.ClipName);
+				soundSource.FadeOut();
+			}
+		}
+
+		private static void OnMusicChangeState(SoundSource source)
+		{
+			if (source.State == ESoundSourceState.FadeOut)
+			{
+				if (autoPlayNext)
+					PlayMusic(null);
+			}
+		}
+
+		public static float MusicVolume
+		{
+			get => musicVolume;
+			set
+			{
+				musicVolume = value;
+				var soundSource = SoundSystem.GetSource(musicHandle);
+				if (soundSource != null && soundSource.IsNotCompleted)
+					soundSource.Volume = musicVolume;
+			}
+		}
+	}
+}
+
